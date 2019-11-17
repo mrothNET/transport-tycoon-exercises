d9d6fa54a8edbef041e69fe921b4fa6c2732e1ba
Michael Roth <mail@mroth.net>
2019-11-17 15:35:37 +0100

    Class Location

--- src/exercise2/Cargo.ts
+++ src/exercise2/Cargo.ts
@@ -1,14 +1,15 @@
 import { Destination } from "./Destination";
+import { Location } from "./Location";
 
 let numCargos = 0;
 
 export class Cargo {
   // tslint:disable-next-line: variable-name
   public readonly cargo_id: number;
-  public readonly origin: string;
+  public readonly origin: Location;
   public readonly destination: Destination;
 
-  constructor(origin: string, destination: Destination) {
+  constructor(origin: Location, destination: Destination) {
     this.cargo_id = ++numCargos;
     this.origin = origin;
     this.destination = destination;
---
+++ src/exercise2/Location.ts
@@ -0,0 +1,3 @@
+import { Destination } from "./Destination";
+
+export type Location = "FACTORY" | "PORT" | Destination;
