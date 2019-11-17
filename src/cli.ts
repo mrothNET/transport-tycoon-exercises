94e19b3401f86eb1dd0794ee01d2edbf2d0dfefc
Michael Roth <mail@mroth.net>
2019-11-17 22:12:04 +0100

    CLI option --log <logifle> for exercise 2Writes events to a  file.

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,7 @@
 
 import { bold, redBright } from "chalk";
 import * as program from "commander";
+import { writeFileSync } from "fs";
 import exercise1 from "./exercise1";
 import exercise2 from "./exercise2";
 
@@ -16,12 +17,17 @@ function cmdExercise1(args: string[]) {
   }
 }
 
-function cmdExercise2(arg: string) {
+function cmdExercise2(arg: string, logFile?: string) {
   try {
     const events = exercise2(arg);
+    const lines = events.map(event => JSON.stringify(event));
 
-    for (const event of events) {
-      console.log(JSON.stringify(event));
+    if (logFile) {
+      lines.push("");
+      writeFileSync(logFile, lines.join("\n"));
+      console.log(`${bold(events.length)} events written to: ${bold(logFile)}`);
+    } else {
+      lines.forEach(line => console.log(line));
     }
   } catch (error) {
     console.error(`${arg}: ${redBright(error.message)}`);
@@ -37,7 +43,8 @@ program
 
 program
   .command("exercise2 <cargolist>")
-  .description("print the delivery events for the gove cargo list")
-  .action(cmdExercise2);
+  .description("print the delivery events for the given cargo list")
+  .option("--log <filename>", "writes events to a file")
+  .action((arg, cmd) => cmdExercise2(arg, cmd.log));
 
 program.parse(process.argv);
